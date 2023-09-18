66c3b4ea939d09b821ef65c4ed32564e94145d92
Felipe Megale <felipe.megale17@gmail.com>
2023-09-18 13:28:43 -0400

    annotate store

--- app/store/applicationStore.ts
+++ app/store/applicationStore.ts
@@ -1,11 +1,21 @@
+/**
+ * Import required packages
+ */
 import { Store } from 'pullstate';
 import { DeviceContextInfo } from '../types/DeviceContextInfo';
 import { BSSID } from '../types/BSSID';
 
+/**
+ * Define an object that represents data to be available globally within the application.
+ * Fields can be included or removed as needed.
+ */
 interface UIStore {
     userToken?: string;
     deviceContextInfo?: DeviceContextInfo;
     desiredBSSIDs?: Array<BSSID>;
 }
 
+/**
+ * Creates and exports (makes available) the application store.
+ */
 export const applicationStore = new Store<UIStore>({ userToken: undefined, deviceContextInfo: undefined, desiredBSSIDs: undefined });
